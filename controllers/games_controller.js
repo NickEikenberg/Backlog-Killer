37e6ae309cb341c3e4917a164285e56833f68137
Nick Eikenberg <nickeikenberg95@gmail.com>
2021-10-20 19:14:39 -0400

    Setup seed route

--- controllers/games_controller.js
+++ controllers/games_controller.js
@@ -1,14 +1,40 @@
 const express = require('express');
-const router = express.Router();
+const games = express.Router();
+const Game = require('../models/games');
 
 // SHOW
-router.get('/:titleOfGame', (req, res) => {
+games.get('/:titleOfGame', (req, res) => {
   res.render('games/show.ejs', { tabTitle: req.params.titleOfGame });
 });
 
 // INDEX
-router.get('/', (req, res) => {
-  res.render('games/index.ejs', { tabTitle: 'Home' });
+games.get('/', (req, res) => {
+  Game.find({}, (error, allGames) => {
+    res.render('games/index.ejs', { tabTitle: 'Home', games: allGames });
+  });
 });
 
-module.exports = router;
+// SEED
+games.get('/setup/seed', (req, res) => {
+  Game.deleteMany({}, () => {});
+  Game.create(
+    [
+      {
+        title: 'Silent Hill 2',
+        releaseDate: 'September 24th, 2001',
+        platform: 'PS2, Xbox',
+        genre: 'Survival Horror',
+        description:
+          'Silent Hill 2 is a 2001 survival horror video game published by Konami for the PlayStation 2 and developed by Team Silent, part of Konami Computer Entertainment Tokyo. The second installment in the Silent Hill series, Silent Hill 2 centers on James Sunderland, a widower who journeys to the town of Silent Hill after receiving a letter from his dead wife informing him that she is waiting there for him. ',
+        inBacklog: false,
+        inNowPlaying: false,
+        inFinished: false,
+      },
+    ],
+    (error, data) => {
+      res.redirect('/');
+    }
+  );
+});
+
+module.exports = games;
---
+++ models/games.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const gameSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    releaseDate: { type: String, required: true },
+    platform: { type: String, required: true },
+    genre: { type: String, required: true },
+    description: { type: String, required: true },
+    inBacklog: Boolean,
+    inNowPlaying: Boolean,
+    inFinished: Boolean,
+  },
+  { timestamps: true }
+);
+
+const Game = mongoose.model('game', gameSchema);
+
+module.exports = Game;
