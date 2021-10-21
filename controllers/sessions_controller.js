17141b862cf808759b665ca97661e8b4ccc8b6f8
Nick Eikenberg <nickeikenberg95@gmail.com>
2021-10-21 14:39:08 -0400

    Sessions get and post complete
    User can now create an account and login to that account

--- controllers/sessions_controller.js
+++ controllers/sessions_controller.js
@@ -7,4 +7,22 @@ sessions.get('/new', (req, res) => {
   res.render('sessions/new.ejs', { tabTitle: 'Login' });
 });
 
+sessions.post('/', (req, res) => {
+  User.findOne({ username: req.body.username }, (err, foundUser) => {
+    if (err) {
+      res.send('There was a problem');
+    } else if (!foundUser) {
+      res.send('<a href="/">No user found</a>');
+    } else {
+      if (bcrypt.compareSync(req.body.password, foundUser.password)) {
+        req.session.currentUser = foundUser;
+        console.log('User Logged In:', foundUser);
+        res.redirect('/backlogkiller');
+      } else {
+        res.send('<a href="/">Password does not match</a>');
+      }
+    }
+  });
+});
+
 module.exports = sessions;
