612023d19a4323a490724b03467761d913faa352
Nick Eikenberg <nickeikenberg95@gmail.com>
2021-10-20 20:57:10 -0400

    Player can add a game to their backlog
    Added a button to add a game to the users backlog

--- controllers/games_controller.js
+++ controllers/games_controller.js
@@ -1,16 +1,31 @@
 const express = require('express');
 const games = express.Router();
 const Game = require('../models/games');
+const Seed = require('../models/gamesSeed');
+
+// PATCH
+games.patch('/:id', (req, res) => {
+  Game.findByIdAndUpdate(req.params.id, req.body, (err, game) => {
+    console.log(game);
+    res.redirect(`/backlogkiller/${game.name}`);
+  });
+});
 
 // SHOW
 games.get('/:titleOfGame', (req, res) => {
-  Game.find({});
+  //   res.render('games/show.ejs', { tabTitle: req.params.titleOfGame });
+  Game.find({ name: req.params.titleOfGame }, (err, game) => {
+    res.render('games/show.ejs', {
+      tabTitle: game.title,
+      game: game,
+      name: req.params.titleOfGame,
+    });
+  });
 });
 
 // INDEX
 games.get('/', (req, res) => {
   Game.find({}, (error, allGames) => {
-    console.log(allGames);
     res.render('games/index.ejs', { tabTitle: 'Home', games: allGames });
   });
 });
@@ -18,26 +33,9 @@ games.get('/', (req, res) => {
 // SEED
 games.get('/setup/seed', (req, res) => {
   Game.deleteMany({}, () => {});
-  Game.create(
-    [
-      {
-        name: 'silenthill2',
-        title: 'Silent Hill 2',
-        releaseDate: 'September 24th, 2001',
-        platform: 'PS2, Xbox',
-        genre: 'Survival Horror',
-        description:
-          'Silent Hill 2 is a 2001 survival horror video game published by Konami for the PlayStation 2 and developed by Team Silent, part of Konami Computer Entertainment Tokyo. The second installment in the Silent Hill series, Silent Hill 2 centers on James Sunderland, a widower who journeys to the town of Silent Hill after receiving a letter from his dead wife informing him that she is waiting there for him. ',
-        img: 'https://images.igdb.com/igdb/image/upload/t_cover_big/co2vyg.png',
-        inBacklog: false,
-        inNowPlaying: false,
-        inFinished: false,
-      },
-    ],
-    (error, data) => {
-      res.redirect('/');
-    }
-  );
+  Game.create(Seed, (error, data) => {
+    res.redirect('/');
+  });
 });
 
 // JSON INDEX
