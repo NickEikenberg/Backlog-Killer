2b441f8045c32a7293b0dcf437dc5d623c111e60
Nick Eikenberg <nickeikenberg95@gmail.com>
2021-10-20 20:06:14 -0400

    Fixed bug in games model

--- controllers/games_controller.js
+++ controllers/games_controller.js
@@ -4,12 +4,13 @@ const Game = require('../models/games');
 
 // SHOW
 games.get('/:titleOfGame', (req, res) => {
-  res.render('games/show.ejs', { tabTitle: req.params.titleOfGame });
+  Game.find({});
 });
 
 // INDEX
 games.get('/', (req, res) => {
   Game.find({}, (error, allGames) => {
+    console.log(allGames);
     res.render('games/index.ejs', { tabTitle: 'Home', games: allGames });
   });
 });
@@ -20,12 +21,14 @@ games.get('/setup/seed', (req, res) => {
   Game.create(
     [
       {
+        name: 'silenthill2',
         title: 'Silent Hill 2',
         releaseDate: 'September 24th, 2001',
         platform: 'PS2, Xbox',
         genre: 'Survival Horror',
         description:
           'Silent Hill 2 is a 2001 survival horror video game published by Konami for the PlayStation 2 and developed by Team Silent, part of Konami Computer Entertainment Tokyo. The second installment in the Silent Hill series, Silent Hill 2 centers on James Sunderland, a widower who journeys to the town of Silent Hill after receiving a letter from his dead wife informing him that she is waiting there for him. ',
+        img: 'https://images.igdb.com/igdb/image/upload/t_cover_big/co2vyg.png',
         inBacklog: false,
         inNowPlaying: false,
         inFinished: false,
--- models/games.js
+++ models/games.js
@@ -2,11 +2,13 @@ const mongoose = require('mongoose');
 
 const gameSchema = new mongoose.Schema(
   {
+    name: { type: String, required: true },
     title: { type: String, required: true },
     releaseDate: { type: String, required: true },
     platform: { type: String, required: true },
     genre: { type: String, required: true },
     description: { type: String, required: true },
+    img: { type: String, required: true },
     inBacklog: Boolean,
     inNowPlaying: Boolean,
     inFinished: Boolean,
