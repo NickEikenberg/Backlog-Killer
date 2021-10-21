7cbb99b41c88b1888f29c3ede23252fc13327d7b
Nick Eikenberg <nickeikenberg95@gmail.com>
2021-10-21 13:04:55 -0400

    Create new user complete
    User can create an account

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -7,4 +7,15 @@ users.get('/new', (req, res) => {
   res.render('users/new.ejs', { tabTitle: 'Create Account' });
 });
 
+users.post('/', (req, res) => {
+  req.body.password = bcrypt.hashSync(
+    req.body.password,
+    bcrypt.genSaltSync(10)
+  );
+  User.create(req.body, (err, createdUser) => {
+    console.log('new user: ', createdUser);
+    res.redirect('/backlogkiller');
+  });
+});
+
 module.exports = users;
